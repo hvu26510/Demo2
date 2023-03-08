ecb4676b85fd79b100e741b91a70afd576f22739
hvu26510 <hvu26510@gmail.com>
2023-03-08 15:46:12 +0700

    Tạo project demo3

---
+++ Demo3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, đây là demo project bài 3");
+            Console.ReadLine();
+        }
+    }
+}
